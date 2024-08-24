2fd81ea2d0b561889142d70489751365502ee2b1
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-24 12:41:42 -0300

    token02

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -114,17 +114,17 @@ export const logout = (req, res) => {
     return res.sendStatus(200);
 };
 
-//export const profile = async (req, res) => {
-//    //console.log(req.user);
-//    const userFound = await User.findById(req.user.id);
-//
-//    if (!userFound) return res.status(400).json({ message: "User not found" });
-//
-//    return res.json({
-//        id: userFound._id,
-//        username: userFound.username,
-//        email: userFound.email,
-//        createdAt: userFound.createdAt,
-//        updatedAt: userFound.updatedAt,
-//    });
-//};
+export const profile = async (req, res) => {
+    //console.log(req.user);
+    const userFound = await User.findById(req.user.id);
+
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    });
+};
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,17 +3,20 @@ import { Router } from "express";
 import {
     login,
     logout,
+    profile,
     register,
     verifyToken,
 } from "../controllers/auth.controller.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
+import { auth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
 router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
+router.get("/profile", auth, profile);
 router.get("/verify", verifyToken);
 
 export default router;
