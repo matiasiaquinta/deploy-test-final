8187f9b0aa44801926f3be52a20c9e68abbbbdb1
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-24 14:23:07 -0300

    update token axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { VITE_API_URL } from "../config";
 
+const token = localStorage.getItem("token");
+
 const instance = axios.create({
     baseURL: VITE_API_URL,
     withCredentials: true,
+    headers: {
+        Authorization: `Bearer ${token}`, // Asegúrate de que el token se incluya en los encabezados
+    },
 });
 
 export default instance;
