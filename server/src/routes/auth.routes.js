c0c2c964c1df7fd0eb56c057b3a1fe413e5d3f20
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-24 13:19:34 -0300

    bug fix

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,7 +3,6 @@ import { Router } from "express";
 import {
     login,
     logout,
-    profile,
     register,
     verifyToken,
 } from "../controllers/auth.controller.js";
@@ -16,7 +15,7 @@ const router = Router();
 router.post("/register", validateSchema(registerSchema), register);
 router.post("/login", validateSchema(loginSchema), login);
 router.post("/logout", logout);
-router.get("/profile", auth, profile);
+//router.get("/profile", auth, profile);
 router.get("/verify", verifyToken);
 
 export default router;
